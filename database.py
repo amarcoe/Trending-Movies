8b6be1968c355351332d8c5047d4f37f5ed0b35f
amarcoe <54554581+amarcoe@users.noreply.github.com>
2023-03-30 22:20:34 -0500

    Updated login function, about to deploy

--- database.py
+++ database.py
@@ -12,7 +12,12 @@ class Person(db.Model):
     comments = db.Column(db.String(500), nullable=True)
     ratings = db.Column(db.Integer, nullable=True)
 
+    def __repr__(self) -> str:
+        return f"Person with username: {self.username} created"
+
+    # TODO: Create an authenticator function
+
+
 def create_table(app):
     with app.app_context():
         db.create_all()
-
--- driver.py
+++ driver.py
@@ -2,21 +2,26 @@ import TMDB_API
 import Wiki_API
 import flask
 import database
-from flask_login import LoginManager
+from flask_login import LoginManager, login_required
 
 # from database import create_table, Person, db
 from os import getenv
 
 app = flask.Flask(__name__)
 app.secret_key = getenv("secret_key")
-# app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 
-login_manager = LoginManager()
+# login_manager = LoginManager()
 
-# db.init_app(app)
-login_manager.init_app(app)
+db.init_app(app)
+# login_manager.init_app(app)
 
-# create_table(app)
+create_table(app)
+
+
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.get(user_id)
 
 
 @app.route("/")
@@ -26,26 +31,25 @@ def login():
 
 @app.route("/login", methods=["POST"])
 def handle_login():
-    form_data = flask.request.form  # This will give us all the form data from index
+    form_data = flask.request.form
     username = form_data["username"]
-    if authenticate_user(username):
-        return flask.redirect(flask.url_for("index"))
-    else:
-        flask.flash(
-            "That username doesn't exit, try typing it again or create an account."
-        )
-        return flask.redirect(flask.url_for("login"))
 
-
-# 1.Login queries against database username
-# 1. If username is in database continue
-# 2. Else flash message
-# 2.If user clicks create account
-# 1. Query against database for username
-# 2. If not in database
-# 1.Add and continue
-# 3. Else
-# 1. flash message saying to login
+    if "login" in flask.request.form:
+        if authenticate(username):
+            return flask.redirect(flask.url_for("index"))
+        else:
+            flask.flash(
+                "That username doesn't exit, try typing it again or create an account."
+            )
+            return flask.redirect(flask.url_for("login"))
+    elif "create_account" in flask.request.form:
+        create_account = form_data["create_account"]
+        if authenticate(username):
+            flask.flash("That account already exists, please login")
+            return flask.redirect(flask.url_for("login"))
+        # else:
+        # Create account
+        # Need active database to continue
 
 
 @app.route("/index")
@@ -57,8 +61,4 @@ def index():
     )
 
 
-def authenticate_user(username):
-    return username == "Alex"
-
-
-app.run(debug=True)
+# app.run(debug=True)
